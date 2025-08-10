e9a3f1506d91459a532acb1e9b9be5790e552472
Ana-Hasan <adhdgeek7@gmail.com>
2025-08-10 17:58:13 +0530

    push

--- assignment1.cpp
+++ assignment1.cpp
@@ -17,6 +17,38 @@ int main(){
     cin>> c;
     f = (9*c)/5 + 32; //note - You can’t use a variable’s value before assigning something to it. When a variable is declared but not initialized, it has indeterminate data (whatever was in that memory space before).
     cout<<"Temp in fahrenheit: "<< f<< endl;
+
+    //question3
+    cout << "Standard for loop:" << endl;
+    for (int i = 0; i < 5; i++)
+        cout << i << " ";
+    
+    cout << "\nFor loop without initialization:" << endl;
+    int i = 0;
+    for (; i < 5; i++)
+        cout << i << " ";
+
+    cout << "\nFor loop without increment:" << endl;
+    i = 0;
+    for (; i < 5;) {
+        cout << i << " ";
+        i++;
+    }
+
+    cout << "\nWhile loop:" << endl;
+    i = 0;
+    while (i < 5) {
+        cout << i << " ";
+        i++;
+    }
+
+    cout << "\nDo-While loop:" << endl;
+    i = 0;
+    do {
+        cout << i << " ";
+        i++;
+    } while (i < 5);
+
     return 0;
 }
 
