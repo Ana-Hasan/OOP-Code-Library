3888ae384b7cee5c31abbd46e3ebc1e7c5763e60
Ana-Hasan <adhdgeek7@gmail.com>
2025-08-10 17:32:11 +0530

    commit1

---
+++ assignment1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// question1
+int main(){
+    cout << "hello world" << endl;
+    cout <<"line1\nline2" << endl;
+    cout <<"column1\tcolumn2" << endl;
+    cout <<"alarm\a" << endl;
+     cout << "Start\rReplace" << endl;
+    return 0;
+}
