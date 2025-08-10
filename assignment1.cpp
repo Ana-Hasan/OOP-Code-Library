a642670e1d03d65e0264e549f33e57a300e18d16
Ana-Hasan <adhdgeek7@gmail.com>
2025-08-10 17:44:29 +0530

    commits made

--- assignment1.cpp
+++ assignment1.cpp
@@ -3,10 +3,20 @@ using namespace std;
 
 // question1
 int main(){
+    // question1
     cout << "hello world" << endl;
     cout <<"line1\nline2" << endl;
     cout <<"column1\tcolumn2" << endl;
     cout <<"alarm\a" << endl;
      cout << "Start\rReplace" << endl;
+
+    // question2
+    double c;
+    double f;
+    cout<< "enter temp in celsius: ";
+    cin>> c;
+    f = (9*c)/5 + 32; //note - You can’t use a variable’s value before assigning something to it. When a variable is declared but not initialized, it has indeterminate data (whatever was in that memory space before).
+    cout<<"Temp in fahrenheit: "<< f<< endl;
     return 0;
 }
+
